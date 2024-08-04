b09e152da9d63263ecf07a417eb2d39f6649a92c
tminz177 <154902646+tminz177@users.noreply.github.com>
2024-08-04 17:48:23 +0700

    init git

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using DemoCapstone.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DemoCapstone.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyStoreContext _context;
+
+        [BindProperty]
+        public Staff Staff { get; set; } = default!;
+        public IndexModel(MyStoreContext context)
+        {
+            _context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var data = _context.Staffs.Where(s => s.Name.Equals(Staff.Name) && s.Password.Equals(Staff.Password)).ToList();
+                if (data.Count() > 0)
+                {
+                    return Redirect("/Home");
+                }
+                else
+                {
+                    TempData["error"] = "Login failed";
+                    return RedirectToAction("/Index");
+                }
+            }
+            return RedirectToAction("/Index");
+        }
+    }
+}
